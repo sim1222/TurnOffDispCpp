71b6bbe2ac203d90e4c4f4bb6f01318ea8fa8114
sim1222 <kokt@sim1222.com>
2022-03-29 13:25:04 +0900

    first commit

---
+++ TurnOffDispCpp/TurnOffDispCpp.cpp
@@ -0,0 +1,37 @@
+﻿// TurnOffDispCpp.cpp : このファイルには 'main' 関数が含まれています。プログラム実行の開始と終了がそこで行われます。
+//
+
+#include <iostream>
+#include <Windows.h>
+
+#define WM_SYSCOMMAND   0x0112
+#define SC_MONITORPOWER 0xF170
+
+int main()
+{
+    HWND hWnd = GetForegroundWindow();
+    UINT Msg = WM_SYSCOMMAND;
+    WPARAM wParam = SC_MONITORPOWER;
+    LPARAM lParam = 2;
+	try
+	{
+		std::cout << "Trying turn off screens...\n";
+		Sleep(1000);
+		PostMessage(hWnd, Msg, wParam, lParam);
+	}
+	catch (std::exception e)
+	{
+		std::cout << "Error: " << e.what() << std::endl;
+	}
+}
+
+// プログラムの実行: Ctrl + F5 または [デバッグ] > [デバッグなしで開始] メニュー
+// プログラムのデバッグ: F5 または [デバッグ] > [デバッグの開始] メニュー
+
+// 作業を開始するためのヒント: 
+//    1. ソリューション エクスプローラー ウィンドウを使用してファイルを追加/管理します 
+//   2. チーム エクスプローラー ウィンドウを使用してソース管理に接続します
+//   3. 出力ウィンドウを使用して、ビルド出力とその他のメッセージを表示します
+//   4. エラー一覧ウィンドウを使用してエラーを表示します
+//   5. [プロジェクト] > [新しい項目の追加] と移動して新しいコード ファイルを作成するか、[プロジェクト] > [既存の項目の追加] と移動して既存のコード ファイルをプロジェクトに追加します
+//   6. 後ほどこのプロジェクトを再び開く場合、[ファイル] > [開く] > [プロジェクト] と移動して .sln ファイルを選択します
